ba6d26fabb52843d659d208291e4b99cf50b7e69
d0p1 <contact@d0p1.eu>
2025-12-02 11:25:37 +0100

    doc: add README.md and document some functions in sconf.h

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# libsconf
+<div align="center">
 
+# libsconf - Lightweight S-expression parser 
+
+[![GitHub License](https://img.shields.io/github/license/d0p1s4m4/libsconf?style=flat-square&logo=opensourceinitiative&logoColor=white)
+](LICENSE)
+![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/d0p1s4m4/libsconf/check.yml?branch=master&style=flat-square)
+
+
+</div>
+
+libsconf is a small, simple C99 library to parse and manipulate
+S-expressions.
+
+## Building
+
+```
+$ autoreconf -i -f
+$ ./configure
+$ make
+$ make check # run test suite
+$ sudo make install
+```
+
+## Basic Usage
+
+### Parsing
+
+From a string:
+```c
+const char *str = "(name \"libsconf\")";
+
+struct sconf *root = sconf_parse(str);
+sconf_dump(stdout, root);
+/* → (name "libsconf") */
+
+sconf_destroy(root);
+```
+
+From a file:
+```c
+FILE *fp = fopen("config.conf", "r");
+struct sconf *root = sconf_load(fp);
+fclose(fp);
+
+if (root == NULL)
+{
+	fprintf(stderr, "error: %s\n",
+		sconf_error_str(sconf_get_last_error()));
+}
+```
+
+### Creating expressions manually
+
+```c
+struct sconf *lst = sconf_new_list();
+struct sconf *name = sconf_new_symbol("username");
+struct sconf *str = sconf_new_string("d0p1");
+
+sconf_list_appends(lst, name, str, SCONF_END);
+
+sconf_dump(stdout, lst);
+/* → (username "d0p1") */
+
+sconf_destroy(lst);
+```
+
+## License
+
+<img src="https://opensource.org/wp-content/themes/osi/assets/img/osi-badge-light.svg" align="right" height="128px" alt="OSI Approved License">
+
+libsconf is licensed under the 3-Clause BSD License.
+
+The full text of the license can be accessed via [this link](https://opensource.org/licenses/BSD-3-Clause) and is also included in [the license file](LICENSE) of this software package.
--- sconf.h
+++ sconf.h
@@ -3,6 +3,14 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+
+/**
+ * \file sconf.h
+ * \brief Lightweith S-expression parsing library.
+ *
+ * libsconf provides a simple interface to parse, inspect, create,
+ * manipulate S-expressions.
+ */
 #ifndef SCONF_H
 # define SCONF_H 1
 
@@ -16,50 +24,99 @@ extern "C"
 {
 # endif /* __cplusplus */
 
+/**
+ * \brief Marks termination in variadic list append calls.
+ */
 # define SCONF_END ((struct sconf *)0)
 
+/**
+ * \enum sconf_error
+ * \brief Error codes returned by libsconf.
+ */
 enum sconf_error {
-	SCONF_OK = 0,
-	SCONF_ERR_MALLOC,
-	SCONF_ERR_OUTOFBOUND,
-	SCONF_ERR_NOTALIST,
+	SCONF_OK = 0,          /**< No error */
+	SCONF_ERR_MALLOC,      /**< Memory allocation failed */
+	SCONF_ERR_OUTOFBOUND,  /**< Index out of range */
+	SCONF_ERR_NOTALIST,    /**< Value is not a list */
 };
 
+/**
+ * \enum sconf_bool
+ * \brief Boolean values.
+ */
 enum sconf_bool {
 	SCONF_FALSE = 0,
 	SCONF_TRUE  = 1,
 };
 
+/**
+ * \enum sconf_type
+ * \brief Type of an S-expression object.
+ */
 enum sconf_type {
-	SCONF_T_NIL,
-	SCONF_T_LIST,
-	SCONF_T_STRING,
-	SCONF_T_CHAR,
-	SCONF_T_INT,
-	SCONF_T_DOUBLE,
-	SCONF_T_SYMBOL,
-	SCONF_T_BOOL
+	SCONF_T_NIL,    /**< Empty value */
+	SCONF_T_LIST,   /**< List of S-expression */
+	SCONF_T_STRING, /**< Null-terminated string */
+	SCONF_T_CHAR,   /**< Single character */
+	SCONF_T_INT,    /**< Integer number */
+	SCONF_T_DOUBLE, /**< Floating-point number */
+	SCONF_T_SYMBOL, /**< Symbol identifier */
+	SCONF_T_BOOL    /**< Boolean */
 };
 
+/**
+ * \struct sconf
+ * \brief Represents a single S-expression object.
+ */
 struct sconf {
-	enum sconf_type type;
+	enum sconf_type type; /**< object type */
 
-	struct sconf *next;
-	struct sconf *prev;
+	struct sconf *next;   /**< next element in list */
+	struct sconf *prev;   /**< previous element in list */
 
 	union {
-		char *as_string;
-		int as_int;
-		double as_double;
-		struct sconf *as_child;
+		char *as_string;        /**< For strings and symbols */
+		int as_int;             /**< For integers and booleans */
+		double as_double;       /**< For floating-point numbers */
+		struct sconf *as_child; /**< For list first elements */
 	} value;
 };
 
+/**
+ * \brief Get library version as a constant string (eg: 1.0.0).
+ * \return Null-terminated version string
+ */
 const char *sconf_version(void);
+
+/**
+ * \brief Parse S-expression from an open FILE stream.
+ * \param fp input file pointer
+ * \return Parsed object or NULL on error.
+ */
 struct sconf *sconf_load(FILE *fp);
+
+/**
+ * \brief Parse S-expression from a null-terminated string.
+ * \param str input null-terminated string
+ * \return Parsed object or NULL on error.
+ */
 struct sconf *sconf_parse(const char *str);
+
+/**
+ * \brief Parse S-expression from buffer with length.
+ * \param str input buffer
+ * \param len buffer length
+ * \return Parsed object or NULL on error.
+ */
 struct sconf *sconf_parse_with_len(const char *str, size_t len);
+
+/**
+ * \brief Pretty-print an S-expression to a stream.
+ * \param fp output stream
+ * \param sexp S-expression.
+ */
 void sconf_dump(FILE *fp, const struct sconf *sexp);
+
 struct sconf *sconf_new_list(void);
 struct sconf *sconf_new_symbol(const char *sym);
 struct sconf *sconf_new_string(const char *str);
@@ -70,14 +127,66 @@ struct sconf *sconf_new_bool(enum sconf_bool b);
 struct sconf *sconf_new_true(void);
 struct sconf *sconf_new_false(void);
 struct sconf *sconf_new_nil(void);
+
+/**
+ * \brief Append an object to a list.
+ * \param lst list root
+ * \param itm element to add
+ * \return 0 on success.
+ */
 int sconf_list_append(struct sconf *lst, struct sconf *itm);
+
+/**
+ * \brief Append multiple objects to a list.
+ * \param lst list root
+ * \param ... elements to add, ending with SCONF_END
+ * \return 0 on success.
+ */
 int sconf_list_appends(struct sconf *lst, ...);
+
+/**
+ * \brief Remove an element from a list (not destroyed).
+ * \param lst list root
+ * \param itm element to remove
+ * \return 0 on success.
+ */
 int sconf_list_remove(struct sconf *lst, struct sconf *itm);
+
+/**
+ * \brief Count list elements.
+ * \param lst list root
+ * \return number of elements.
+ */
 int sconf_list_size(const struct sconf *lst);
+
+/**
+ * \brief Retrive element at index.
+ * \param lst list root
+ * \param idx index
+ * \return Object or NULL if out of range.
+ */
 struct sconf *sconf_list_at(const struct sconf *lst, int idx);
+
+/**
+ * \brief Return the first element of a list.
+ * \param lst list root
+ * \return Object or NULL on empty list.
+ */
 struct sconf *sconf_list_first(struct sconf *lst);
+
+/**
+ * \brief Free an S-expression object.
+ */
 void sconf_destroy(struct sconf *sexp);
+
+/**
+ * \brief Get the last error code.
+ */
 enum sconf_error sconf_get_last_error(void);
+
+/**
+ * \brief Convert an error code to a readable string.
+ */
 const char *sconf_error_str(enum sconf_error err);
 
 static inline int
